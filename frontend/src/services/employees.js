7ba86173c2fa174f43cc4a36345a50d457d72df4
Ezzeddine Ben Hadj Yahya <ezzeddine.hadjyahya@hitema.eu>
2022-08-04 14:20:45 +0200

    removed clg

--- frontend/src/services/employees.js
+++ frontend/src/services/employees.js
@@ -5,7 +5,6 @@ export default function useemployees() {
   const errors = ref("");
 
   const storeEmployee = async (data) => {
-    console.log(data);
     errors.value = "";
     try {
       await axios.post("http://127.0.0.1:8000/api/create_employee", data);
